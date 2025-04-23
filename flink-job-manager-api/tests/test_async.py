8787997244cdfe9fd4b8bb1f2f1919ebc49ac155
shijing <resink.ai@yahoo.com>
2025-04-23 09:07:21 -0700

    Release version 1.0a20250423

--- flink-job-manager-api/tests/test_async.py
+++ flink-job-manager-api/tests/test_async.py
@@ -1,20 +1,62 @@
+import json
+import time
 import unittest
+import asyncio
 
-from flink_job_manager_api import Client
-from flink_job_manager_api.api.default import get_cluster_overview, get_dashboard_configuration
-
-BASE_URL = "http://localhost:8081"
+from flink_gateway_api import Client
+from flink_gateway_api.api.default import (
+    get_api_version,
+    get_info,
+    open_session,
+    close_session,
+    execute_statement,
+    fetch_results,
+)
+from flink_gateway_api.models import (
+    OpenSessionRequestBody,
+    ExecuteStatementResponseBody,
+    RowFormat,
+)
 
 
 class TestAsyncCalls(unittest.IsolatedAsyncioTestCase):
-    async def test_dashboard_configuration(self):
-        async with Client(BASE_URL) as client:
-            response = await get_dashboard_configuration.asyncio(client=client)
-            self.assertIsNotNone(response.to_dict())
-            print(f"dashboard configuration response: {response.to_dict()}")
+    async def test_api_version(self):
+        async with Client('http://localhost:8083') as client:
+            response = await get_api_version.asyncio(client=client)
+            self.assertEqual(response.versions, ["V1", "V2"])
+            print(response.to_dict())
 
-    async def test_cluster_overview(self):
-        async with Client(BASE_URL) as client:
-            response = await get_cluster_overview.asyncio(client=client)
+    async def test_info(self):
+        async with Client('http://localhost:8083') as client:
+            response = await get_info.asyncio(client=client)
             self.assertIsNotNone(response.to_dict())
-            print(f"cluster overview response: {response.to_dict()}")
+            print(f"info response: {response.to_dict()}")
+
+    async def test_execute(self):
+        async with Client('http://localhost:8083') as client:
+            responses = await open_session.asyncio(client=client, body=OpenSessionRequestBody.from_dict({
+                "properties": {
+                    "idle-timeout": "10s"
+                },
+                "sessionName": "test_session"
+            }))
+            print(f"Open session response: {responses}")
+
+            select_result = await execute_statement.asyncio(responses.session_handle, client=client,
+                                                            body=ExecuteStatementResponseBody.from_dict({
+                                                                "statement": "SELECT 23 as age, 'Alice Liddel' as name;",
+                                                            }))
+
+            print(f"Select result: {select_result}")
+            await asyncio.sleep(1)
+            fetch_return = await fetch_results.asyncio(
+                responses.session_handle,
+                select_result.operation_handle,
+                0,
+                client=client,
+                row_format=RowFormat.JSON,
+            )
+            print(f"Fetch return: {json.dumps(fetch_return.to_dict())}")
+
+            await close_session.asyncio(responses.session_handle, client=client)
+            print(f"Session closed")
